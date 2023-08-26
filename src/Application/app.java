bd19ac99db37430a1caf41dd940175c3a2e15758
Klinger Marinho <klingercm@hotmail.com>
2023-08-26 18:21:19 -0400

    DAo FActory

--- src/Application/app.java
+++ src/Application/app.java
@@ -2,6 +2,8 @@ package Application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,7 +15,12 @@ public class app {
 		System.out.println(obj);
 		
 		Seller seller = new Seller(21,"KLINGER TESTE","KLINGERCM@HOTMAIL.COM", new Date(), 15000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println(seller);
+		Seller seller1 = new Seller(201,"KLINGER MARINHO","KLINGERCM@HOTMAIL.COM", new Date(), 15000.0, obj);
+		sellerDao.insert(seller1);
+		System.out.println(seller1);
 
 	}
 
