636900d549acead705adbfcdfe0eab6e16a0102d
Klinger Marinho <klingercm@hotmail.com>
2023-08-26 17:59:35 -0400

    Classe Seller

--- src/Application/app.java
+++ src/Application/app.java
@@ -1,6 +1,9 @@
 package Application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class app {
 
@@ -8,8 +11,9 @@ public class app {
 		// TODO Auto-generated method stub
 		Department obj = new Department(1,"Testes");
 		System.out.println(obj);
-	
 		
+		Seller seller = new Seller(21,"KLINGER TESTE","KLINGERCM@HOTMAIL.COM", new Date(), 15000.0, obj);
+		System.out.println(seller);
 
 	}
 
