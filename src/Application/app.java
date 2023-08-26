36c110fc8af7794433c74e10687a4c135d9369e9
Klinger Marinho <klingercm@hotmail.com>
2023-08-26 17:34:42 -0400

    Projeto com dao demo-dao-jdbc

---
+++ src/Application/app.java
@@ -0,0 +1,16 @@
+package Application;
+
+import model.entities.Department;
+
+public class app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Department obj = new Department(1,"Testes");
+		System.out.println(obj);
+	
+		
+
+	}
+
+}
